a68ae2470fdf699a676b98fa29e65ee521e0c00a
Jimmy Kalubi <kalubijimmy12@gmail.com>
2020-12-01 22:42:31 +0100

    feat: adding Migartion for its app

---
+++ backend/database/migrations/2020_12_01_211013_create_accounts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('password');
+            $table->string('acount_type');
+            $table->string('picture')->nullable();
+            $table->string('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
