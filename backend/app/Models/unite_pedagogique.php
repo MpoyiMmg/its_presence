4a0e1aa26d935ffd1dce7c91dd0854ddbfde88bc
Jimmy Kalubi <kalubijimmy12@gmail.com>
2020-12-09 21:22:37 +0100

    fix: model fix filiable method

--- backend/app/Models/unite_pedagogique.php
+++ backend/app/Models/unite_pedagogique.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class unite_pedagogique extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'designation', 'is_deleted'
+    ];
 }
